5d10a7fdf4cf12d6338edc038c10a6e1ae064043
Priti Gupta <civilstudent9900@gmail.com>
2024-08-07 23:48:18 +0530

    second commit

---
+++ src/helpers/sendVerificaationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+
+    await resend.emails.send({
+      from: 'you@example.com',
+      to: email,
+      subject: 'Secret Sender | Verification Code',
+      react: VerificationEmail({username, otp: verifyCode}),
+    });
+
+    return {success: true, message: "verification email dend successfully"}
+  } catch (emailError) {
+    console.error("Error sending verification email", emailError)
+    return {success: false, message: "Failed to send verification email"}
+  }
+}
